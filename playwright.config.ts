4cd8ca5f833a40335414329ec5a53de152e0ac8a
qaninja8876 <vincecarter8876@gmail.com>
2025-09-16 03:53:13 +0400

    Add GitHub Actions workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    [
+      'allure-playwright',
+      {
+        outputFolder: 'allure-results',
+        suiteTitle: 'Discogs Tests'
+      }
+    ]
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
